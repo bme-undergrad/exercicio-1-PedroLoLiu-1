55ee4f73a77235b5776b77cd7b0516cc7a1aa765
PedroLoLiu <151867668+PedroLoLiu@users.noreply.github.com>
2025-09-02 14:35:54 -0300

    exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -1,20 +1,26 @@
-function t = exercicio1(func, func_d, x0)
+function t = exercicio1(func,func_d,x0)
 
-es = 0.01;  
-imax = 20;  
+% nao alterar: inicio
+es = 0.01;
+imax = 20;
+% nao alterar: fim
 
-t_roots = zeros(imax, 1);
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+t_roots = zeros(imax,1);
 t_roots(1) = x0;
 
-for ii = 1:lenght(t_roots)-1
+for ii = 1:length(t_roots)-1
   if ii ~= 1
     erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii))
     if erro(ii) < es
       break
     endif
   endif
-    t_roots(ii+1) = t_roots(ii) - func(t_roots(ii))/func_d(t_roots(ii));
-endfor
-t = t_roots(ii);
- 
+    t_roots(ii+1) = t_roots(ii) - func(t_roots(ii))/func_d(t_roots(ii))
+endfor 
+t = t_roots(ii); 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 endfunction
